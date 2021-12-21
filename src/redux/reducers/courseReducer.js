81b7cee3280f5b094064b577e4c994587b73342f
Mangala <m@mangala.dev>
2021-12-21 21:51:10 +1100

    Added first action and reducers.

---
+++ src/redux/reducers/courseReducer.js
@@ -0,0 +1,8 @@
+export default function courseReducer(state = [], action) {
+  switch (action.type) {
+    case "CREATE_COURSE":
+      return [...state, { ...action.courseReducer }];
+    default:
+      return state;
+  }
+}
